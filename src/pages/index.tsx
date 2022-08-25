04776ae7e0f4d8df3da226d6d51fd4faafccc065
abhrajitray77 <abhrajitray77@gmail.com>
2022-08-25 21:10:08 +0530

    day1 complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,8 +28,7 @@ function index({
   trendingNow,
   }:Props) {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10
-                    to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Netflix</title>
         <link href="/favicon.ico" rel="icon" type="image/x-icon"/>
@@ -38,7 +37,7 @@ function index({
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals}/>
-        <section>
+        <section className="md:space-y-24">
         <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
