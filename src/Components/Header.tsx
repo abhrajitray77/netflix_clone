63018b18ba9341a3259c979e6ea268c67de961c5
abhrajitray77 <abhrajitray77@gmail.com>
2022-08-26 07:51:27 +0530

    dep2

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -27,12 +27,11 @@ function Header() {
   return (
     <header className={`${isScrolled && 'bg-[#141414]'}`}>
         <div className="flex items-center space-x-2 md:space-x-10">
-            <Image className=" cursor-pointer object-contain"
+            <img className=" cursor-pointer object-contain"
                 src= "https://rb.gy/ulxxee"
-                layout='intrinsic'
                 width={100}
-                height={40}
-                alt="assa"
+                height={100}
+                alt="netflix"
             />
             <ul className="hidden space-x-4 md:flex ">
                 <li className="headerLink"> Home </li>
